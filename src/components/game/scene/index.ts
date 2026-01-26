491ffeeb5de81f393912fb80569a98dfb0be39e5
Yue Zhou@Paraflux <yue@paraflux.ca>
2026-01-26 10:32:15 -0700

    checkpoint: modify src/components/game/scene/index.ts - 10:32

---
+++ src/components/game/scene/index.ts
@@ -0,0 +1,7 @@
+export { SceneView } from "./SceneView";
+export { NegotiationPanel } from "./NegotiationPanel";
+export { SceneActionMenu } from "./SceneActionMenu";
+export { IntensityMeter } from "./IntensityMeter";
+export { ParticipantList } from "./ParticipantList";
+export { SafewordButton } from "./SafewordButton";
+export { AftercareView } from "./AftercareView";
