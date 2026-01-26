c7438a58067f4800ed3794548fe50f3ef9607d8a
Yue Zhou@Paraflux <yue@paraflux.ca>
2026-01-26 10:29:11 -0700

    checkpoint: modify src/components/game/combat/index.ts - 10:29

---
+++ src/components/game/combat/index.ts
@@ -0,0 +1,6 @@
+export { CombatView } from "./CombatView";
+export { CombatGrid } from "./CombatGrid";
+export { CombatToken } from "./CombatToken";
+export { InitiativeTracker } from "./InitiativeTracker";
+export { ActionBar } from "./ActionBar";
+export { MovementOverlay, calculateValidMoves, calculateAttackRange } from "./MovementOverlay";
