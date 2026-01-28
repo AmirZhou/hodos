bcaa8910e094c22d4f1f9c64735fd3810c4bf3eb
Yue Zhou@Paraflux <yue@paraflux.ca>
2026-01-28 13:45:03 -0700

    checkpoint: modify src/app/(main)/campaigns/[campaignId]/characters/new/page.tsx - 13:45

--- src/app/(main)/campaigns/[campaignId]/characters/new/page.tsx
+++ src/app/(main)/campaigns/[campaignId]/characters/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { useMutation } from "convex/react";
 import { api } from "../../../../../../../convex/_generated/api";
