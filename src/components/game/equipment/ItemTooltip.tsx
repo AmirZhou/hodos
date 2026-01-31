16de6607bad2a989af2ba798df998d8a0315f5b2
Yue Zhou@Paraflux <yue@paraflux.ca>
2026-01-31 08:44:23 -0700

    checkpoint: modify src/components/game/equipment/ItemTooltip.tsx - 08:44

--- src/components/game/equipment/ItemTooltip.tsx
+++ src/components/game/equipment/ItemTooltip.tsx
@@ -4,14 +4,16 @@ import { getRarityColor, getRarityLabel, getSlotLabel, formatStatName, formatSta
 import type { EquipmentSlot, Rarity } from "../../../../convex/data/equipmentItems";
 
 interface ItemData {
-  id: string;
+  _id: string;
   name: string;
   description: string;
-  type: EquipmentSlot;
-  rarity: Rarity;
+  type: string;
+  rarity: string;
   stats: Record<string, number | string | undefined>;
   specialAttributes?: Record<string, number | undefined>;
   passive?: string;
+  bindingRule?: string;
+  boundTo?: string;
 }
 
 export function ItemTooltip({ item }: { item: ItemData }) {
