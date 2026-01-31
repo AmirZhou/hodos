0b1a689e791ba665a9e163273b21331f5362e875
Yue Zhou@Paraflux <yue@paraflux.ca>
2026-01-31 16:33:08 -0700

    checkpoint: modify convex/characters.ts - 16:33

--- convex/characters.ts
+++ convex/characters.ts
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { requireAuth, requireCampaignMember, requireCharacterOwner } from "./lib/auth";
 
 const defaultSkills: Record<string, number> = {
   // Strength
