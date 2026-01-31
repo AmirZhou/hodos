a683bf022da42b4518d2fec750f2d733ef49c0b8
Yue Zhou@Paraflux <yue@paraflux.ca>
2026-01-31 16:33:23 -0700

    checkpoint: modify convex/npcs.ts - 16:33

--- convex/npcs.ts
+++ convex/npcs.ts
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { requireCampaignMember } from "./lib/auth";
 
 const defaultAdultStats = {
   composure: 75,
