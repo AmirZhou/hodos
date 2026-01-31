36c3b341a8a45f2bbaed677d74464596648bf84c
Yue Zhou@Paraflux <yue@paraflux.ca>
2026-01-31 16:34:29 -0700

    checkpoint: modify convex/game/loot.ts - 16:34

--- convex/game/loot.ts
+++ convex/game/loot.ts
@@ -2,6 +2,7 @@ import { v } from "convex/values";
 import { mutation, query } from "../_generated/server";
 import { createItemInstance, logItemEvent } from "../equipment";
 import { getBindingRule } from "../data/equipmentItems";
+import { requireCampaignMember, requireCharacterOwner } from "../lib/auth";
 
 // ============ QUERIES ============
 
